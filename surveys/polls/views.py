f7f6bc4966b1f8d03cf1653238ca1b68457fe717
Samuel Sublate <samuelsublate.dev@gmail.com>
2023-09-18 18:25:08 -0300

    feat: implemented generic views

--- surveys/polls/views.py
+++ surveys/polls/views.py
@@ -1,6 +1,36 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.db.models import F
+
+from surveys.polls.models import Question, Choice
 
 
-def index(request):
-    return HttpResponse("init")
+class IndexView(generic.ListView):
+    template_name = 'index.html'
+    context_object_name = 'latest_question_list'
+    
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+    
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'results.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'detail.html', {'question': question, 'error_message': "You didn't select a choice."})
+    
+    selected_choice.update(votes=F('votes') + 1)
+    return HttpResponseRedirect(reverse('polls:result', args=(question_id,)))
