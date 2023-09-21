e691021051eee71392a2b9e369692d5faef02a0a
Samuel Sublate <samuelsublate.dev@gmail.com>
2023-09-21 12:01:32 -0300

    feat: implemented debug bar urls

--- surveys/urls.py
+++ surveys/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, reverse
+from django.shortcuts import redirect
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: redirect(reverse('polls:index'))),
     path('polls/', include('surveys.polls.urls')),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
